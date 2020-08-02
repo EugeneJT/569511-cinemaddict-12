1e04ddfdd1ff70bc0a540fea8d4fae5a03c6c204
Eugene Nasonov <eugenetikk@gmail.com>
2020-08-03 01:23:39 +0300

    added module2-task1

---
+++ src/view/extra-film.js
@@ -0,0 +1,6 @@
+export const createExtraFilmTemplate = (title) => {
+  return `<section class="films-list--extra">
+      <h2 class="films-list__title">${title}</h2>
+      <div class="films-list__container"></div>
+    </section>`;
+};
